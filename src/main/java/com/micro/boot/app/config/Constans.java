f0e31eeab585268446d883c620233ccc0ee9d43d
huliang <418206020@qq.com>
2018-03-23 00:49:10 +0800

    constants

--- src/main/java/com/micro/boot/app/config/Constans.java
+++
@@ -1,7 +0,0 @@
-package com.micro.boot.app.config;
-
-public class Constans {
-
-
-
-}
---
+++ src/main/java/com/micro/boot/common/Constants.java
@@ -0,0 +1,27 @@
+/**
+ * Created by 418206020 on 2018-03-21.
+ */
+
+package com.micro.boot.common;
+
+/**
+ * 〈系统常量〉
+ *
+ * @author Administrator
+ * @create 2018/3/23
+ * @since 1.0.0
+ */
+public class Constants {
+
+    /**
+     * APP发布版本
+     */
+    public static final String VERSION_APP = "1.0";
+
+    /**
+     * WEB版本
+     */
+    public static final String VERSION_WEB = "1.0";
+    
+
+}
